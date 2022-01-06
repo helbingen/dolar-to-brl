e0277a9262fd030af248b3522695f56b4de87034
Daniel Helbingen <48421336+helbingen@users.noreply.github.com>
2022-01-06 19:41:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL";
-// fetch(url).then((res) => {
-//     return res.json()
-// }).then((data) => {
-//     console.log(data.USDBRL.ask);
-//     let dolar = data.USDBRL.ask;
-// })
-
 function converter() {
     let input = document.getElementById("valor");
     let valor = input.value;
-    let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL";
+    let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
     fetch(url)
         .then((res) => {
             return res.json()
