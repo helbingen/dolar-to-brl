2a4f356eb1bef3296af85e8d1442f90221aa3da0
Daniel Helbingen <48421336+helbingen@users.noreply.github.com>
2022-01-06 19:34:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL";
+// fetch(url).then((res) => {
+//     return res.json()
+// }).then((data) => {
+//     console.log(data.USDBRL.ask);
+//     let dolar = data.USDBRL.ask;
+// })
+
+function converter() {
+    let input = document.getElementById("valor");
+    let valor = input.value;
+    let url = "http://economia.awesomeapi.com.br/json/last/USD-BRL";
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data.USDBRL.ask);
+            let dolar = data.USDBRL.ask;
+            document.getElementById("resultado").innerHTML = valor + " dólar = " + (dolar * valor) + " reais.";
+            document.getElementById("cotacao").innerHTML = "Cotação atual: 1 Dólar americano igual a " + dolar + " Reais brasileiros.";
+        })
+}
